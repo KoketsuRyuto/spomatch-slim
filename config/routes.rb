01fd83556a7b8793829ed5e90c366425e04a0655
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-05-11 23:21:39 +0900

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :user, controllers: {
+  devise_for :users, controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
