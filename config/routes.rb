546863fc867a252fdc792ceb81dd16008cbddb92
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-05-14 23:40:48 +0900

    [Fix] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -10,27 +10,27 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/' => 'users#index'
-    resources :users, only: %(show edit update)
-    resources :sports, only: %(index create edit update destroy)
-    resources :tags, only: %(index create edit update)
-    resources :posts, only: %(index show destroy)
-    resources :post_comments, only: %(index show destroy)
+    resources :users, only: ["show", "edit", "update"]
+    resources :sports, only: ["index", "create", "edit", "update", "destroy"]
+    resources :tags, only: ["index", "create", "edit", "update"]
+    resources :posts, only: ["index", "show", "destroy"]
+    resources :post_comments, only: ["index", "show", "destroy"]
   end
 
   scope module: :public do
     root to: 'homes#top'
     get 'top' => 'homes#about'
-    resources :users, only: %(show edit update)
+    resources :users, only: ["show", "edit" ,"update"]
     get 'users/:id/confilm' => 'users#confilm', as: 'users/confilm'
     patch 'users/:id/withdraw' => 'users#withdraw', as: 'users/withdraw'
     resources :posts do
-      resources :post_comments, only: %(create destroy)
-      resource :favorites, only: %(create destroy)
+      resources :post_comments, only: ["create", "destroy"]
+      resource :favorites, only: ["create", "destroy"]
     end
     get 'search_tag/:tag_id' => 'posts#search_tag', as: 'search_tag'
     get 'search_sport/:sport_id' => 'groups#search_sport', as: 'search_sport'
     resources :groups do
-      resources :group_chats, only: %(show create)
+      resources :group_chats, only: ["show", "create"]
       get 'join' => 'groups#join'
       delete 'withdraw' => 'groups#withdraw'
     end
