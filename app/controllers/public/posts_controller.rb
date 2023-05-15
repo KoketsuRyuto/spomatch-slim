774d8d5274b0652f4cec748cece81592a530a5b6
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-05-15 11:38:52 +0900

    [Add]投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -10,17 +10,36 @@ class Public::PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(update_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redierct_to posts_path
   end
 
   private
 
   def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def update_params
     params.require(:post).permit(:title, :body)
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,4 +6,24 @@ class Public::UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update(update_params)
+    redirect_to user_path(@user)
+  end
+
+  def confilm
+    
+  end
+
+  def withdraw
+    
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
